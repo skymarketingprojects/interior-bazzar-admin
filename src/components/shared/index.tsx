900c709a0517e29c188c31deb5941e2e2642d7a0
Aman Pandey <skytechno00@gmail.com>
2025-09-26 15:53:16 +0530

    asfd

--- src/components/shared/index.tsx
+++ src/components/shared/index.tsx
@@ -1,6 +1,5 @@
 import Error from "./Toast/Error"
 import Thanks from "./Toast/Thanks";
 import Success from "./Toast/Toast"
-import ContactCard from "./Contact/ContactCard";
 
-export { ContactCard, Success, Error, Thanks };
+export { Success, Error, Thanks };

25c5dfd33ae14cc92e3aff489db989fd4f849712
Aman Pandey <skytechno00@gmail.com>
2025-09-30 17:41:00 +0530

    some changes done

--- src/components/dashboard/Lead/LeadDetail/index.tsx
+++ src/components/dashboard/Lead/LeadDetail/index.tsx
@@ -4,22 +4,6 @@ import type { AdminLeadType } from "../../../../types/content";
 import styles from "./LeadDetail.module.css";
 
 const LeadDetail = ({ lead }: { lead: AdminLeadType }) => {
-    const queryText = String(lead.query ?? "");
-    const intrestedIn = String(lead.interested ?? "");
-
-    const renderMultiline = (text: string) => {
-        const lines = text.split(/\r?\n/);
-        return (
-            <td className={styles.multiline}>
-                {lines.map((line, i) => (
-                    <span key={i}>
-                        {line}
-                        {i < lines.length - 1 && <br />}
-                    </span>
-                ))}
-            </td>
-        );
-    };
 
     return (
         <div className={styles.wrapper}>
@@ -32,17 +16,14 @@ const LeadDetail = ({ lead }: { lead: AdminLeadType }) => {
                     <tr><th>Email</th><td>{lead.email}</td></tr>
                     <tr><th>Phone</th><td>{lead.phone}</td></tr>
                     <tr><th>Interested In</th><td>{lead.interested}</td></tr>
-
                     <tr>
                         <th>Query</th>
-                        {renderMultiline(queryText)}
+                        <td>{lead.query}  </td>
                     </tr>
-
                     <tr>
                         <th>Intrested</th>
-                        {renderMultiline(intrestedIn)}
+                        <td>{lead.interested}</td>
                     </tr>
-
                     <tr><th>Country</th><td>{lead.country}</td></tr>
                     <tr><th>City</th><td>{lead.city}</td></tr>
                     <tr><th>Assigned To</th><td>{lead.assigned}</td></tr>
